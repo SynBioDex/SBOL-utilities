593de5f227c2d83fa8b7c1fd107fb4ea8958af5c
Jake Beal <jakebeal@gmail.com>
2021-08-02 18:22:17 -0500

    Add test for configurable parsing

--- test/test_excel_to_sbol.py
+++ test/test_excel_to_sbol.py
@@ -16,6 +16,7 @@ TESTFILE_DIR = os.path.join(
 
 logging.getLogger().setLevel(level=logging.DEBUG)
 
+
 def test_conversion():
     wb = openpyxl.load_workbook(TESTFILE_DIR + '/simple_library.xlsx', data_only=True)
     sbol3.set_namespace('http://sbolstandard.org/testfiles/')
@@ -29,7 +30,37 @@ def test_conversion():
 
     temp_name = tempfile.gettempdir() + '/' + next(tempfile._get_candidate_names())
     doc.write(temp_name, sbol3.SORTED_NTRIPLES)
-    assert_files_identical(temp_name, TESTFILE_DIR + '/simple_library.nt')\
+    assert_files_identical(temp_name, TESTFILE_DIR + '/simple_library.nt')
+
+
+def test_custom_conversion():
+    wb = openpyxl.load_workbook(TESTFILE_DIR + '/nonstandard_simple_library.xlsx', data_only=True)
+    sbol3.set_namespace('http://sbolstandard.org/testfiles/')
+    config = {
+        'basic_parts_name': 'C2',
+        'basic_parts_description': 'A12',
+        'basic_first_row': 21,
+        'basic_row_col': 2,
+        'basic_notes_col': 3,
+        'basic_description_col': 5,
+        'basic_source_prefix_col': 6,
+        'basic_source_id_col': 7,
+        'basic_final_col': 10,
+        'basic_circular_col': 11,
+        'basic_length_col': 12,
+        'basic_sequence_col': 13
+    }
+    doc = sbol_utilities.excel_to_sbol.excel_to_sbol(wb,config)
+
+    assert not doc.validate().errors and not doc.validate().warnings
+    assert len(doc.find('BasicParts').members) == 25
+    assert len(doc.find('CompositeParts').members) == 6
+    assert len(doc.find('LinearDNAProducts').members) == 2
+    assert len(doc.find('FinalProducts').members) == 2
+
+    temp_name = tempfile.gettempdir() + '/' + next(tempfile._get_candidate_names())
+    doc.write(temp_name, sbol3.SORTED_NTRIPLES)
+    assert_files_identical(temp_name, TESTFILE_DIR + '/simple_library.nt')
 
 
 def test_constraints():
