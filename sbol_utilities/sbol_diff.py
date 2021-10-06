5b27a6d52e4f5390e3d305b758a6053236025307
Tom Mitchell <tcmitchell@users.noreply.github.com>
2021-10-06 15:39:36 -0400

    Add sbol-diff
    Add a command line program to difference two SBOL files. Uses only RDF
    differencing so it will work across RDF formats.

---
+++ sbol_utilities/sbol_diff.py
@@ -0,0 +1,84 @@
+import argparse
+import logging
+import os
+import sys
+import time
+from typing import Union, Tuple
+
+import rdflib.compare
+
+
+def init_logging(debug=False):
+    msg_format = "%(asctime)s.%(msecs)03dZ:%(levelname)s:%(message)s"
+    date_format = "%Y-%m-%dT%H:%M:%S"
+    level = logging.INFO
+    if debug:
+        level = logging.DEBUG
+    logging.basicConfig(format=msg_format, datefmt=date_format, level=level)
+    logging.Formatter.converter = time.gmtime
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file1', metavar='FILE1',
+                        help='First Input File')
+    parser.add_argument('file2', metavar='FILE2',
+                        help='Second Input File')
+    parser.add_argument('-s', '--silent', action='store_true',
+                        help='Generate no output, only status')
+    parser.add_argument('--debug', action='store_true',
+                        help='Enable debug logging (default: disabled)')
+    args = parser.parse_args(args)
+    return args
+
+
+def load_rdf(fpath: Union[str, bytes, os.PathLike]) -> rdflib.Graph:
+    rdf_format = rdflib.util.guess_format(fpath)
+    graph1 = rdflib.Graph()
+    graph1.parse(fpath, format=rdf_format)
+    return graph1
+
+
+def diff_rdf(g1: rdflib.Graph, g2: rdflib.Graph) -> Tuple[rdflib.Graph, rdflib.Graph, rdflib.Graph]:
+    iso1 = rdflib.compare.to_isomorphic(g1)
+    iso2 = rdflib.compare.to_isomorphic(g2)
+    rdf_diff = rdflib.compare.graph_diff(iso1, iso2)
+    return rdf_diff
+
+
+def report_triples(header, graph):
+    print(header)
+    for s, p, o in graph:
+        print(f'\t{s}, {p}, {o}')
+
+
+def report_diffs(fpath1: str, in1: rdflib.Graph,
+                 fpath2: str, in2: rdflib.Graph) -> None:
+    if in1:
+        header = f'Triples in {fpath1}, not in {fpath2}:'
+        report_triples(header, in1)
+    if in2:
+        header = f'Triples in {fpath2}, not in {fpath1}:'
+        report_triples(header, in2)
+
+
+def sbol_diff(fpath1: str, fpath2: str, silent=False) -> int:
+    sbol1 = load_rdf(fpath1)
+    sbol2 = load_rdf(fpath2)
+    _, in1, in2 = diff_rdf(sbol1, sbol2)
+    if not in1 and not in2:
+        return 0
+    else:
+        if not silent:
+            report_diffs(fpath1, in1, fpath2, in2)
+        return 1
+
+
+def main(argv=None):
+    args = parse_args(argv)
+    init_logging(args.debug)
+    return sbol_diff(args.file1, args.file2, silent=args.silent)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ test/test_sbol_diff.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import unittest
+from unittest.mock import patch
+
+import sbol_utilities.sbol_diff
+
+TEST_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_FILES_DIR = os.path.join(TEST_DIR, 'test_files')
+SL_SBOL_PATH = os.path.join(TEST_FILES_DIR, 'simple_library.nt')
+ESL_SBOL_PATH = os.path.join(TEST_FILES_DIR, 'expanded_simple_library.nt')
+
+
+class TestSbolDiff(unittest.TestCase):
+
+    def test_command_line(self):
+        test_args = ['sbol_diff', ESL_SBOL_PATH, ESL_SBOL_PATH]
+        # Diff the same file, expecting no differences
+        with patch.object(sys, 'argv', test_args):
+            status = sbol_utilities.sbol_diff.main()
+        self.assertEqual(0, status)
+        # Diff two different files, use -s silent mode to prevent
+        # unsightly output to terminal
+        test_args = ['sbol_diff', '-s', ESL_SBOL_PATH, SL_SBOL_PATH]
+        with patch.object(sys, 'argv', test_args):
+            status = sbol_utilities.sbol_diff.main()
+        self.assertEqual(1, status)
+
+    def test_sbol_diff(self):
+        # Invoke sbol_utilities.sbol_diff.sbol_diff directly
+        actual = sbol_utilities.sbol_diff.sbol_diff(ESL_SBOL_PATH,
+                                                    ESL_SBOL_PATH,
+                                                    silent=True)
+        expected = 0
+        self.assertEqual(expected, actual)
+        actual = sbol_utilities.sbol_diff.sbol_diff(ESL_SBOL_PATH,
+                                                    SL_SBOL_PATH,
+                                                    silent=True)
+        expected = 1
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
