a467f9d0cc195bb571b64f732163ec50d8591d4a
Jake Beal <jakebeal@gmail.com>
2021-10-10 15:56:25 -0500

    Another set of typing fixes

--- test/helpers.py
+++ test/helpers.py
@@ -1,12 +1,14 @@
 import tempfile
 import os
 from shutil import copy
+from typing import List, Dict
 
 
-def copy_to_tmp(package: list[str] = None, renames: dict[str,str] = None) -> str:
+def copy_to_tmp(package: List[str] = None, renames: Dict[str, str] = None) -> str:
     """Copy test files into a temporary package directory
 
-    :param package: files to go into the package directory
+    :param package: files to go into the temporary package directory
+    :param renames: dictionary of files to be renamed when copied, mapping old name to new
     :return: temporary package directory
     """
     # make a temporary package directory
@@ -21,6 +23,6 @@ def copy_to_tmp(package: list[str] = None, renames: dict[str,str] = None) -> str
     test_dir = os.path.dirname(os.path.realpath(__file__))
     for f in package:
         copy(os.path.join(test_dir, 'test_files', f), tmp_sub)
-    for old_f,new_f in renames.items():
+    for old_f, new_f in renames.items():
         copy(os.path.join(test_dir, 'test_files', old_f), os.path.join(tmp_sub, new_f))
     return tmp_sub
