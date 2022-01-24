89a2c58aec3b53e73e1de34459bc59772c857609
hscott <hscott@bbn.com>
2022-01-24 08:55:39 -0500

    fix importing

--- sbol_utilities/define_module.py
+++ sbol_utilities/define_module.py
@@ -2,14 +2,73 @@ import argparse
 import logging
 
 import sbol3
-import package # TODO: Suppress the output list
+# FIXME: Make sure relative importing works with the intended  use case
+# These ones SHOULD work
+import sbol_utilities.package # TODO: Suppress the output list
+from sbol_utilities.workarounds import type_to_standard_extension
+# These ones SHOULDN'T WORK
+# import package # TODO: Suppress the output list
+# from workarounds import type_to_standard_extension
 
-# TODO: Generalize importing to work in the intended use, right now this works 
-# for calling command line functions from within the directory
-from workarounds import type_to_standard_extension
-# This is the exact same as in the other functions, should work for the actual
-# version
-# from sbol_utilities.workarounds import type_to_standard_extension
+def define_module(doc: sbol3.Document):
+    # Call check_namespace
+    logging.info('Checking namespaces')
+    is_module, module_namespace = check_namespaces(doc)
+    logging.info(f'SBOL Document is a Module: {is_module}')
+
+    # If all namespaces are the same, add module and save as new file
+    if is_module:
+        # TODO: Check if the file already has a module defined, if so, abort
+
+        # Get list of all members 
+        # TODO: Check I want the names and not somthing else, sequences have no names, will modules have no sequences
+        all_names = [o.name for o in doc.objects]
+
+        # Define the module
+        # Module is expecting only one positional argument, 'identity'
+        # TODO: Check what is an identity string, can I just call it 'module'? Do we want it to be user defined?
+        module = sbol_utilities.package.sep_054.Module('Module') # Identity becomes "'http://sbols.org/unspecified_namespace/Module'", do I need to set a namespace, the namespace of the Module?
+        # All top level objects are members
+        module.members = all_names
+        # The displayId should be module, unless it is also a package # TODO: Check if it is also a package
+        # module.display_id = 'module' # Seems to be set automatically
+        # Namespace must match the hasNamespace value for all of its members
+        module.namespace = module_namespace
+
+        # Add the module to the document
+        doc.add(module)
+
+        # TODO: Add the hasModule object too?
+
+        # Write out the file
+        doc.write(outfile_name, file_type)
+        logging.info('Module file written to '+ outfile_name)
+
+def check_namespaces(doc: sbol3.Document):
+    """ Check if the namespaces of all top level objects are the same
+
+    :param doc: Document containing top level objects
+    :return: boolean, true is all namespaces are the same
+    """
+
+    # Make a placeholder for the namespace
+    namespace_holder = None
+
+    # Loop through all TopLevel objects
+    for o in doc.objects:
+
+        # Save the first namespace
+        if namespace_holder is None:
+            namespace_holder = o.namespace
+
+        # Compare every other namespace to the first
+        if o.namespace == namespace_holder:
+            continue
+        else:
+            return False
+
+    # If all namespaces are the same, return true
+    return True, namespace_holder
 
 def main():
     """
@@ -45,63 +104,9 @@ def main():
     doc = sbol3.Document()
     doc.read(sbol_file)
 
-    # Call check_namespace
-    logging.info('Checking namespaces')
-    is_module, module_namespace = check_namespaces(doc)
-    logging.info(f'SBOL Document is a Module: {is_module}')
-
-    ## For debugging ##
-    print(f'SBOL Document is a Module: {is_module}')
-
-    # If all namespaces are the same, add module and save as new file
-    if is_module:
-        # TODO: Check if the file already has a module defined, if so, abort
-
-        # Get list of all members % TODO: Check I want the names and not somthing else
-        all_names = [o.name for o in doc.objects]
-
-        # Define the module
-        module = package.sep_054.Module('Module', all_names) # TODO: Check what is an identity string, can I just call it 'module'? Do we want it to be user defined?
-
-        # The displayId should be module, unless it is also a package # TODO: Check if it is also a package
-        module.displayId('module')
-        # Namespace must match the hasNamespace value for all of its members
-        module.hasNamespace(module_namespace)
-
-        # Add the module to the document
-        doc.add(module)
-
-        # TODO: Add the hasModule object too?
-
-        # Write out the file
-        doc.write(outfile_name, file_type)
-        logging.info('Module file written to '+outfile_name)
-
-def check_namespaces(doc: sbol3.Document):
-    """ Check if the namespaces of all top level objects are the same
-
-    :param doc: Document containing top level objects
-    :return: boolean, true is all namespaces are the same
-    """
-
-    # Make a placeholder for the namespace
-    namespace_holder = None
-
-    # Loop through all TopLevel objects
-    for o in doc.objects:
-
-        # Save the first namespace
-        if namespace_holder is None:
-            namespace_holder = o.namespace
-
-        # Compare every other namespace to the first
-        if o.namespace == namespace_holder:
-            continue
-        else:
-            return False
-
-    # If all namespaces are the same, return true
-    return True, namespace_holder
+    # Call define_module
+    define_module(doc)
+    
 
 if __name__ == '__main__':
     main()
--- test/test_define_module.py
+++ test/test_define_module.py
@@ -1,59 +1,43 @@
 import filecmp
-import sys
-import tempfile
 import unittest
 import os
-from unittest.mock import patch
 
-import openpyxl
 import sbol3
 
-import sbol_utilities.calculate_sequences
-from sbol_utilities.excel_to_sbol import excel_to_sbol
-from sbol_utilities.expand_combinatorial_derivations import expand_derivations
-
+# FIXME: Need help with relative importing
+# I think it's supposed to be 
+# import sbol_utilities.define_module
+# But this gives me an error, ModuleNotFoundError: No module named 'sbol_utilities.define_module'
+# Also tried
+# import define_module
+# from sbol_utilities import define_module
+import sys
+sys.path.append('/c/Users/hscott/Documents/SBOL/SBOL-utilities/')
+import sbol_utilities.define_module
 
 class TestDefineModule(unittest.TestCase):
     def test_define_module(self):
         """Test defining a module from an SBOL document"""
-        # sbol3.set_namespace('http://sbolstandard.org/testfiles') # Is this what I want?
+        
+        # Read in the test file
         test_dir = os.path.dirname(os.path.realpath(__file__))
         doc = sbol3.Document()
-        doc.read(os.path.join(test_dir, 'test_files', 'module.nt'))
-        prior_sequence_count = len([o for o in doc.objects if isinstance(o, sbol3.Sequence)])
-        new_seqs = sbol_utilities.calculate_sequences.calculate_sequences(doc)
-        tmp_out = tempfile.mkstemp(suffix='.nt')[1]
-        doc.write(tmp_out, sbol3.SORTED_NTRIPLES)
+        doc.read(os.path.join(test_dir, 'test_files', 'module_in.nt'))
 
-        # check to see if all of the expected sequences have been filled in as anticipated
-        # total number of new sequences should be: 20
-        #   2018 Interlab: none - missing vector, prior parts all pasted in for vector
-        #   FPs small: none with missing vector, 2x9 = 18 for insert combinations
-        #   Round 1 order, All FPs: none - libraries
-        #   UNSX-UP, BB-B0032-BB: 1 each = 2
-        assert len(new_seqs) == 20, f'Expected 20 new sequences, but found {len(new_seqs)}'
-        sequence_count = len([o for o in doc.objects if isinstance(o, sbol3.Sequence)])
-        assert sequence_count - prior_sequence_count == len(new_seqs)
-        # spot-check a couple of sequence lengths
+        # Run the function
+        sbol_utilities.define_module.define_module(doc)
 
-        # run it again: no additional sequences should get computed
-        new_seqs = sbol_utilities.calculate_sequences.calculate_sequences(doc)
-        second_sequence_count = len([o for o in doc.objects if isinstance(o, sbol3.Sequence)])
-        assert not new_seqs and sequence_count == second_sequence_count, f'Unexpected new sequences {new_seqs}'
+        # Read in the new file
+        new_doc = sbol3.Document()
+        new_doc.read(os.path.join(test_dir, 'test_files', 'out.nt')) # FIXME: Where does my file go by default?
 
-        # make sure that what came out is exactly what was expected
-        comparison_file = os.path.join(test_dir, 'test_files', 'expanded_with_sequences.nt')
-        assert filecmp.cmp(tmp_out, comparison_file), f'Converted file {tmp_out} is not identical'
+        # Compare it to the saved results file, make sure they are the same
+        comparison_file = os.path.join(test_dir, 'test_files', 'module_out.nt')
+        assert filecmp.cmp(new_doc, comparison_file), 'Files are not identical'
 
-    def test_commandline(self):
-        test_dir = os.path.dirname(os.path.realpath(__file__))
-        temp_name = tempfile.mkstemp(suffix='.nt')[1]
-        test_args = ['sbol-calculate-sequences', '-vv',
-                     os.path.join(test_dir, 'test_files', 'module.nt'), '-o', temp_name]
-        with patch.object(sys, 'argv', test_args):
-            sbol_utilities.calculate_sequences.main()
-        comparison_file = os.path.join(test_dir, 'test_files', 'expanded_with_sequences.nt')
-        assert filecmp.cmp(temp_name, comparison_file), f'Converted file {temp_name} is not identical'
+        # Delete the file
+        if os.path.exists('out.nt'):
+            os.remove('out.nt')
 
 if __name__ == '__main__':
     unittest.main()
