b0fb10c4e4261c6d0ab5dcc7e6911fde8a45727e
hscott <hscott@bbn.com>
2022-01-27 17:48:02 -0500

    restore python 3.7

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='sbol-utilities',
             # Specify the Python versions you support here. In particular, ensure
             # that you indicate whether you support Python 2, Python 3 or both.
             'Programming Language :: Python :: 3 :: Only',
-            # 'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9'
       ],
