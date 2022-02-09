9b8d412a27b31f29333dee46ef1369e890aa475a
Helen Scott <helengracescott@gmail.com>
2022-02-08 19:18:39 -0500

    remove dev file

--- dev.py
+++
@@ -1,8 +0,0 @@
-all_namespaces = ["grace", "graceful",
-           "disgraceful", "gracefully"]
-
-stem = "gro"
-
-# print(all(x == all_namespaces[0] for x in all_namespaces))
-
-print(all(stem in namespace for namespace in all_namespaces))
