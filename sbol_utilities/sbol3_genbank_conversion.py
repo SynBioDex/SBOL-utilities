86e1998443ad700f3f00f70146e159d97386535e
mohitdmak <mdmohitrc@gmail.com>
2022-06-23 19:16:56 +0530

    add: parsing capabilities for genbanks with multiple records and features

--- sbol_utilities/sbol3_genbank_conversion.py
+++ sbol_utilities/sbol3_genbank_conversion.py
@@ -1,23 +1,30 @@
 import os
 import sbol3
 from Bio import GenBank, SeqIO
+from sbol3.constants import SBOL_SEQUENCE_FEATURE
 GENBANK_PARSER = GenBank.RecordParser()
 
 # Conversion Constants (NOTE: most are placeholding and temporary for now)
-SAMPLE_GB_FILE_1 = "BBa_J23101.gb"          
-# Simplest file in genbank consisting only of a singular sequence
+SAMPLE_GB_FILE_1 = "BBa_J23101.gb"
 SAMPLE_SBOL3_FILE_1 = "BBa_J23101_from_genbank_to_sbol3_direct.nt"
+SAMPLE_GB_FILE_2 = "iGEM_SBOL2_imports.gb"
+SAMPLE_SBOL3_FILE_2 = "iGEM_SBOL2_imports_from_genbank_to_sbol3_direct.nt"
 # Temporarily assuming only dna components to be dealt with in genbank files
-COMP_TYPES = [sbol3.SBO_DNA]                
+COMP_TYPES = [sbol3.SBO_DNA]
 # Temporarilty assuming components to only have the engineered_region role
-COMP_ROLES = [sbol3.SO_ENGINEERED_REGION]   
+COMP_ROLES = [sbol3.SO_ENGINEERED_REGION]
 GENBANK_FILE_1 = os.path.join(os.path.abspath(os.path.curdir),
-                    "test/test_files", SAMPLE_GB_FILE_1)
+                              "test/test_files", SAMPLE_GB_FILE_1)
 SBOL3_FILE_1 = os.path.join(os.path.abspath(os.path.curdir),
-                    "test/test_files", SAMPLE_SBOL3_FILE_1)
+                            "test/test_files", SAMPLE_SBOL3_FILE_1)
+GENBANK_FILE_2 = os.path.join(os.path.abspath(os.path.curdir),
+                              "test/test_files", SAMPLE_GB_FILE_2)
+SBOL3_FILE_2 = os.path.join(os.path.abspath(os.path.curdir),
+                            "test/test_files", SAMPLE_SBOL3_FILE_2)
 
-def convert_genbank_to_sbol3(gb_file: str, sbol3_file: str, 
-                    namespace: str = "https://testing.sbol3.genbank/", write: bool = False):
+
+def convert_genbank_to_sbol3(gb_file: str, sbol3_file: str,
+                             namespace: str = "https://testing.sbol3.genbank/", write: bool = False):
     # create sbol3 document, and record parser handler for gb file
     sbol3.set_namespace(namespace)
     doc = sbol3.Document()
@@ -27,24 +34,35 @@ def convert_genbank_to_sbol3(gb_file: str, sbol3_file: str,
         # NOTE: Currently we assume only linear or circular topology is possible
         COMP_TYPES.append(sbol3.SO_LINEAR if record.annotations['topology'] == "linear" else sbol3.SO_CIRCULAR)
         comp = sbol3.Component(identity=record.name,
-                        types=COMP_TYPES,
-                        roles=COMP_ROLES,
-                        description=record.description)
+                               types=COMP_TYPES,
+                               roles=COMP_ROLES,
+                               description=record.description)
         doc.add(comp)
         # NOTE: Currently we use a single method of encoding (IUPAC)
         seq = sbol3.Sequence(identity=record.name + "_sequence",
-                        elements=str(record.seq.lower()),
-                        encoding=sbol3.IUPAC_DNA_ENCODING)
+                             elements=str(record.seq.lower()),
+                             encoding=sbol3.IUPAC_DNA_ENCODING)
         doc.add(seq)
         comp.sequences = [seq]
-    if write: 
+        if record.features:
+            comp.features = []
+            for i in range(len(record.features)):
+                SEQ_FEAT_RANGE_ORIENTATION = "https://identifiers.org/SO:0001030" # inline orientation
+                gb_feat = record.features[i]
+                gb_loc = gb_feat.location
+                # adding roles to feature?
+                locs = sbol3.Range(sequence=seq, start=gb_loc.nofuzzy_start, end=int(gb_loc.end), orientation=SEQ_FEAT_RANGE_ORIENTATION)
+                feat = sbol3.SequenceFeature(locations=[locs], name=gb_feat.qualifiers['label'][0])
+                comp.features.append(feat)
+    if write:
         doc.write(fpath=sbol3_file, file_format=sbol3.SORTED_NTRIPLES)
     return doc
 
 
 # Currently we don't parse input for gb and sbol3 files (hardcoded)
 def main():
-    convert_genbank_to_sbol3(gb_file=GENBANK_FILE_1, sbol3_file=SBOL3_FILE_1, write=True)
+    convert_genbank_to_sbol3(gb_file=GENBANK_FILE_2, sbol3_file=SBOL3_FILE_2, write=True)
+
 
 if __name__ == '__main__':
     main()
--- test/test_genbank_sbol3_direct.py
+++ test/test_genbank_sbol3_direct.py
@@ -2,7 +2,7 @@ import unittest
 import sbol3
 from sbol_utilities.sbol_diff import doc_diff
 from sbol_utilities.sbol3_genbank_conversion import convert_genbank_to_sbol3, \
-    GENBANK_FILE_1, SBOL3_FILE_1
+    GENBANK_FILE_1, SBOL3_FILE_1, GENBANK_FILE_2, SBOL3_FILE_2
 
 class TestGenBank2SBOL3(unittest.TestCase):
     def test_simple_file_1(self):
@@ -16,6 +16,21 @@ class TestGenBank2SBOL3(unittest.TestCase):
         sbol3_file_1.read(location=SBOL3_FILE_1, file_format=sbol3.SORTED_NTRIPLES)
         assert not doc_diff(test_output_sbol3, sbol3_file_1), \
             f'Converted SBOL3 file: {TEST_OUTPUT_SBOL3} not identical to expected file: {SBOL3_FILE_1}'
+
+    def test_simple_file_2(self):
+        """Test conversion of a simple genbank file with a multiple sequence with multiple features"""
+        sbol3.set_namespace("https://testing.sbol3.genbank/")
+        TEST_OUTPUT_SBOL3 = SBOL3_FILE_2 + ".test" 
+        # Don't write to file for testing, we directly compare sbol documents
+        test_output_sbol3 = convert_genbank_to_sbol3(GENBANK_FILE_2, TEST_OUTPUT_SBOL3, 
+                            namespace="https://testing.sbol3.genbank/", write=False)
+        sbol3_file_2 = sbol3.Document()
+        sbol3_file_2.read(location=SBOL3_FILE_2, file_format=sbol3.SORTED_NTRIPLES)
+        print(test_output_sbol3)
+        for obj in test_output_sbol3: 
+            if isinstance(obj, sbol3.Component): print(obj.features)
+        assert not doc_diff(test_output_sbol3, sbol3_file_2), \
+            f'Converted SBOL3 file: {TEST_OUTPUT_SBOL3} not identical to expected file: {SBOL3_FILE_2}'
 
 if __name__ == '__main__':
     unittest.main()
