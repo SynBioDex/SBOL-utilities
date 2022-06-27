75b4a9f39d7ca01a3c64c0f114ce0559e1149779
mohitdmak <mdmohitrc@gmail.com>
2022-06-27 21:20:36 +0530

    add: ontology parsing error handling; cross platform file paths fix; logging; del: tmp scripts; formatting

--- sbol_utilities/sbol3_genbank_conversion.py
+++ sbol_utilities/sbol3_genbank_conversion.py
@@ -11,30 +11,62 @@ TEST_NAMESPACE = "https://test.sbol3.genbank/"
 COMP_TYPES = [sbol3.SBO_DNA]
 # TODO: Temporarily assuming components to only have the engineered_region role
 COMP_ROLES = [sbol3.SO_ENGINEERED_REGION]
-# TODO: Need to find a way to infer SequenceFeature Range/Cut Orientation from the genbank file
-SEQ_FEAT_RANGE_ORIENTATION = "https://identifiers.org/SO:0001030"  # inline orientation
 # TODO: Temporarily encoding sequnce objects in IUPUC mode only
 SEQUENCE_ENCODING = sbol3.IUPAC_DNA_ENCODING
 # Complete file paths to pass as paramteres for testing
-SAMPLE_GENBANK_FILE_1 = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 
-                                     os.pardir, "test/test_files", "BBa_J23101.gb"))
-SAMPLE_GENBANK_FILE_2 = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 
-                                     os.pardir, "test/test_files", "iGEM_SBOL2_imports.gb"))
-SAMPLE_SBOL3_FILE_1 = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 
-                         os.pardir, "test/test_files", "BBa_J23101_from_genbank_to_sbol3_direct.nt"))
-SAMPLE_SBOL3_FILE_2 = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 
-                         os.pardir, "test/test_files", "iGEM_SBOL2_imports_from_genbank_to_sbol3_direct.nt"))
-GB2SO_MAPPINGS_CSV = os.path.join(os.path.dirname(os.path.realpath(__file__)), "gb2so.csv")
-SO2GB_MAPPINGS_CSV = os.path.join(os.path.dirname(os.path.realpath(__file__)), "so2gb.csv")
+SAMPLE_GENBANK_FILE_1 = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.pardir,
+        "test/test_files",
+        "BBa_J23101.gb",
+    )
+)
+SAMPLE_GENBANK_FILE_2 = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.pardir,
+        "test/test_files",
+        "iGEM_SBOL2_imports.gb",
+    )
+)
+SAMPLE_SBOL3_FILE_1 = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.pardir,
+        "test/test_files",
+        "BBa_J23101_from_genbank_to_sbol3_direct.nt",
+    )
+)
+SAMPLE_SBOL3_FILE_2 = os.path.abspath(
+    os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.pardir,
+        "test/test_files",
+        "iGEM_SBOL2_imports_from_genbank_to_sbol3_direct.nt",
+    )
+)
+GB2SO_MAPPINGS_CSV = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "gb2so.csv"
+)
+SO2GB_MAPPINGS_CSV = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "so2gb.csv"
+)
 
 
 class GenBank_SBOL3_Converter:
     gb2so_map = {}
     so2gb_map = {}
     default_SO_ontology = "SO:0000110"
-    default_GB_ontology = "" # TODO: Whats the default here?
-    def create_GB_SO_role_mappings(self, gb2so_csv: str = GB2SO_MAPPINGS_CSV, so2gb_csv: str = SO2GB_MAPPINGS_CSV, 
-                                   convert_gb2so: bool = True, convert_so2gb: bool = True):
+    default_GB_ontology = ""  # TODO: Whats the default here?
+
+    def create_GB_SO_role_mappings(
+        self,
+        gb2so_csv: str = GB2SO_MAPPINGS_CSV,
+        so2gb_csv: str = SO2GB_MAPPINGS_CSV,
+        convert_gb2so: bool = True,
+        convert_so2gb: bool = True,
+    ):
         """Reads 2 CSV Files containing mappings for converting between GenBank and SO ontologies roles
         :param gb2so_csv: path to read genbank to so conversion csv file
         :param so2gb_csv: path to read so to genbank conversion csv file
@@ -42,28 +74,41 @@ class GenBank_SBOL3_Converter:
         :param convert_so2gb: bool stating whether to read csv for so2gb mappings
         """
         if convert_gb2so:
-            logging.info("Reading GB to SO ontology mappings csv")
-            with open(gb2so_csv, mode='r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    self.gb2so_map[row["GenBank_Ontology"]] = row["SO_Ontology"]
+            logging.info(f"Parsing {gb2so_csv} for GenBank to SO ontology mappings.")
+            try:
+                with open(gb2so_csv, mode="r") as csv_file:
+                    csv_reader = csv.DictReader(csv_file)
+                    for row in csv_reader:
+                        self.gb2so_map[row["GenBank_Ontology"]] = row["SO_Ontology"]
+            except FileNotFoundError:
+                logging.error(f"No GenBank to SO Ontology Mapping CSV File Exists!")
+                return 0
         if convert_so2gb:
-            logging.info("Reading SO to GB ontology mappings csv")
-            with open(so2gb_csv, mode='r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    self.so2gb_map[row["SO_Ontology"]] = row["GenBank_Ontology"]
+            logging.info(f"Parsing {so2gb_csv} for SO to GenBank ontology mappings.")
+            try:
+                with open(so2gb_csv, mode="r") as csv_file:
+                    csv_reader = csv.DictReader(csv_file)
+                    for row in csv_reader:
+                        self.so2gb_map[row["SO_Ontology"]] = row["GenBank_Ontology"]
+            except FileNotFoundError:
+                logging.error(f"No SO to Genbank Ontology Mapping CSV File Exists!")
+                return 0
+        return 1
 
-
-    def convert_genbank_to_sbol3(self, gb_file: str, sbol3_file: str = "sbol3.out", namespace: str =
-                                TEST_NAMESPACE, write: bool = False) -> sbol3.Document:
+    def convert_genbank_to_sbol3(
+        self,
+        gb_file: str,
+        sbol3_file: str = "sbol3.out",
+        namespace: str = TEST_NAMESPACE,
+        write: bool = False,
+    ) -> sbol3.Document:
         """Convert a GenBank document on disk into an SBOL3 document
         Specifically, the GenBank document is first imported to SBOL2, then converted from SBOL2 to SBOL3
 
         :param gb_file: path to read GenBank file from
         :param sbol3_file: path to write SBOL3 file to, if write set to true
         :param namespace: URIs of Components will be set to {namespace}/{genbank_id},
-                          defaults to "https://test.sbol3.genbank/" 
+                          defaults to "https://test.sbol3.genbank/"
         :param write: writes the generated sbol3 document in SORTED_NTRIPLES
                           format to provided sbol3_file path
         :return: SBOL3 document containing converted materials
@@ -72,57 +117,92 @@ class GenBank_SBOL3_Converter:
         sbol3.set_namespace(namespace)
         doc = sbol3.Document()
         # create updated py dict to store mappings between gb and so ontologies
-        logging.info("Creating GenBank and SO ontologies mappings for sequence feature roles")
-        self.create_GB_SO_role_mappings(gb2so_csv=GB2SO_MAPPINGS_CSV, convert_so2gb=False)
+        logging.info(
+            "Creating GenBank and SO ontologies mappings for sequence feature roles"
+        )
+        map_created = self.create_GB_SO_role_mappings(
+            gb2so_csv=GB2SO_MAPPINGS_CSV, convert_so2gb=False
+        )
+        if not map_created:
+            logging.critical(
+                f"Required CSV data files are not present in your package.\n    Please reinstall the sbol_utilities package.\n \
+                Stopping current conversion process.\n    Reverting to legacy converter if new Conversion process is not forced."
+            )
+            return None
         # access records by parsing gb file using SeqIO class
-        logging.info("Parsing Genbank records using SeqIO class.")
+        logging.info(
+            f"Parsing Genbank records using SeqIO class.\n    Using GenBank file {gb_file}"
+        )
         for record in list(SeqIO.parse(gb_file, "genbank").records):
             # NOTE: Currently we assume only linear or circular topology is possible
-            if record.annotations['topology'] == "linear":
-                extra_comp_types = [sbol3.SO_LINEAR] 
+            logging.info(f"Parsing record - {record.id} in genbank file.")
+            if record.annotations["topology"] == "linear":
+                extra_comp_types = [sbol3.SO_LINEAR]
             else:
                 extra_comp_types = [sbol3.SO_CIRCULAR]
-            comp = sbol3.Component(identity=record.name,
-                                   types=COMP_TYPES + extra_comp_types,
-                                   roles=COMP_ROLES,
-                                   description=record.description)
+            comp = sbol3.Component(
+                identity=record.name,
+                types=COMP_TYPES + extra_comp_types,
+                roles=COMP_ROLES,
+                description=record.description,
+            )
             doc.add(comp)
             # NOTE: Currently we use a fixed method of encoding (IUPAC)
-            seq = sbol3.Sequence(identity=record.name + "_sequence",
-                                 elements=str(record.seq.lower()),
-                                 encoding=SEQUENCE_ENCODING)
+            seq = sbol3.Sequence(
+                identity=record.name + "_sequence",
+                elements=str(record.seq.lower()),
+                encoding=SEQUENCE_ENCODING,
+            )
             doc.add(seq)
             comp.sequences = [seq]
+            # Sending out warnings for genbank info not storeable in sbol3
+            for keys in record.annotations:
+                logging.warning(
+                    f"Extraneous information not storeable in SBOL3 - {keys}: {record.annotations[keys]}"
+                )
             if record.features:
                 comp.features = []
                 for gb_feat in record.features:
+                    logging.info(
+                        f"Parsing feature {gb_feat.qualifiers['label'][0]} for record {record.id}"
+                    )
                     # create "Range/Cut" FeatureLocation by parsing genbank record location
                     gb_loc = gb_feat.location
                     # Default orientation is "inline" except if complement is specified via strand
-                    feat_orientation = SEQ_FEAT_RANGE_ORIENTATION
-                    if gb_loc.strand == -1: 
-                        feat_orientation = "https://identifiers.org/SO:0001031"
-                    # Create a cut or range as featurelocation depending on whether location is specified as 
+                    feat_orientation = sbol3.SO_FORWARD
+                    if gb_loc.strand == -1:
+                        feat_orientation = sbol3.SO_REVERSE
+                    # Create a cut or range as featurelocation depending on whether location is specified as
                     # Cut (eg: "n^n+1", parsed as [n:n] by biopython) or Range (eg: "n..m", parsed as [n:m] by biopython)
                     if gb_loc.start == gb_loc.end:
-                       # TODO: Biopython parses both "276" (range) and "276^277" (cut) in locations as [276:276], distinction?
-                        locs = sbol3.Cut(sequence=seq, at=int(gb_loc.start), orientation=feat_orientation)
+                        locs = sbol3.Cut(
+                            sequence=seq,
+                            at=int(gb_loc.start),
+                            orientation=feat_orientation,
+                        )
                     else:
-                        locs = sbol3.Range(sequence=seq, start=int(gb_loc.start),
-                                           end=int(gb_loc.end), orientation=feat_orientation)
-                    # TODO: Add defaults below if mappings are not found / key does not exist
+                        locs = sbol3.Range(
+                            sequence=seq,
+                            start=int(gb_loc.start),
+                            end=int(gb_loc.end),
+                            orientation=feat_orientation,
+                        )
                     # Obtain sequence feature role from gb2so mappings
                     feat_role = sbol3.SO_NS
                     if self.gb2so_map.get(gb_feat.type):
                         feat_role += self.gb2so_map[gb_feat.type]
                     else:
                         feat_role += self.default_SO_ontology
-                    feat = sbol3.SequenceFeature(locations=[locs], 
-                                                 roles=[feat_role],
-                                                 name=gb_feat.qualifiers['label'][0])
+                    feat = sbol3.SequenceFeature(
+                        locations=[locs],
+                        roles=[feat_role],
+                        name=gb_feat.qualifiers["label"][0],
+                    )
                     comp.features.append(feat)
         if write:
-            logging.info("Writing created sbol3 document to disk.")
+            logging.info(
+                f"Writing created sbol3 document to disk in sorted ntriples format.\n    With path {sbol3_file}"
+            )
             doc.write(fpath=sbol3_file, file_format=sbol3.SORTED_NTRIPLES)
         return doc
 
@@ -132,8 +212,10 @@ def main():
     log_level = logging.INFO
     logging.getLogger().setLevel(level=log_level)
     converter = GenBank_SBOL3_Converter()
-    converter.convert_genbank_to_sbol3(gb_file=SAMPLE_GENBANK_FILE_2, sbol3_file=SAMPLE_SBOL3_FILE_2, write=True)
+    converter.convert_genbank_to_sbol3(
+        gb_file=SAMPLE_GENBANK_FILE_2, sbol3_file=SAMPLE_SBOL3_FILE_2, write=True
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test/test_genbank_sbol3_direct.py
+++ test/test_genbank_sbol3_direct.py
@@ -2,38 +2,68 @@ import unittest
 import sbol3
 from pathlib import Path
 from sbol_utilities.sbol_diff import doc_diff
-from sbol_utilities.sbol3_genbank_conversion import GenBank_SBOL3_Converter, TEST_NAMESPACE
+from sbol_utilities.sbol3_genbank_conversion import (
+    GenBank_SBOL3_Converter,
+    TEST_NAMESPACE,
+)
+
 
 class TestGenBank2SBOL3(unittest.TestCase):
     # Create converter instance
     converter = GenBank_SBOL3_Converter()
+
     def test_simple_file_1(self):
         """Test conversion of a simple genbank file with a single sequence"""
-        SAMPLE_GENBANK_FILE_1 = str(Path(__file__).parent) + "/test_files/" + "BBa_J23101.gb"
-        SAMPLE_SBOL3_FILE_1 = str(Path(__file__).parent) + "/test_files/" + "BBa_J23101_from_genbank_to_sbol3_direct.nt"
+        SAMPLE_GENBANK_FILE_1 = Path(__file__).parent / "test_files" / "BBa_J23101.gb"
+        SAMPLE_SBOL3_FILE_1 = (
+            Path(__file__).parent
+            / "test_files"
+            / "BBa_J23101_from_genbank_to_sbol3_direct.nt"
+        )
         sbol3.set_namespace(TEST_NAMESPACE)
-        TEST_OUTPUT_SBOL3 = SAMPLE_SBOL3_FILE_1 + ".test" 
+        TEST_OUTPUT_SBOL3 = str(SAMPLE_SBOL3_FILE_1) + ".test"
         # Don't write to file for testing, we directly compare sbol documents
-        test_output_sbol3 = self.converter.convert_genbank_to_sbol3(SAMPLE_GENBANK_FILE_1, TEST_OUTPUT_SBOL3, 
-                            namespace=TEST_NAMESPACE, write=False)
+        test_output_sbol3 = self.converter.convert_genbank_to_sbol3(
+            str(SAMPLE_GENBANK_FILE_1),
+            TEST_OUTPUT_SBOL3,
+            namespace=TEST_NAMESPACE,
+            write=False,
+        )
         sbol3_file_1 = sbol3.Document()
-        sbol3_file_1.read(location=SAMPLE_SBOL3_FILE_1, file_format=sbol3.SORTED_NTRIPLES)
-        assert not doc_diff(test_output_sbol3, sbol3_file_1), \
-            f'Converted SBOL3 file: {TEST_OUTPUT_SBOL3} not identical to expected file: {SAMPLE_SBOL3_FILE_1}'
+        sbol3_file_1.read(
+            location=str(SAMPLE_SBOL3_FILE_1), file_format=sbol3.SORTED_NTRIPLES
+        )
+        assert not doc_diff(
+            test_output_sbol3, sbol3_file_1
+        ), f"Converted SBOL3 file: {TEST_OUTPUT_SBOL3} not identical to expected file: {SAMPLE_SBOL3_FILE_1}"
 
     def test_simple_file_2(self):
         """Test conversion of a simple genbank file with a multiple sequence with multiple features"""
-        SAMPLE_GENBANK_FILE_2 = str(Path(__file__).parent) + "/test_files/" + "iGEM_SBOL2_imports.gb"
-        SAMPLE_SBOL3_FILE_2 = str(Path(__file__).parent) + "/test_files/" + "iGEM_SBOL2_imports_from_genbank_to_sbol3_direct.nt"
+        SAMPLE_GENBANK_FILE_2 = (
+            Path(__file__).parent / "test_files" / "iGEM_SBOL2_imports.gb"
+        )
+        SAMPLE_SBOL3_FILE_2 = (
+            Path(__file__).parent
+            / "test_files"
+            / "iGEM_SBOL2_imports_from_genbank_to_sbol3_direct.nt"
+        )
         sbol3.set_namespace(TEST_NAMESPACE)
-        TEST_OUTPUT_SBOL3 = SAMPLE_SBOL3_FILE_2 + ".test" 
+        TEST_OUTPUT_SBOL3 = str(SAMPLE_SBOL3_FILE_2) + ".test"
         # Don't write to file for testing, we directly compare sbol documents
-        test_output_sbol3 = self.converter.convert_genbank_to_sbol3(SAMPLE_GENBANK_FILE_2, TEST_OUTPUT_SBOL3, 
-                            namespace=TEST_NAMESPACE, write=False)
+        test_output_sbol3 = self.converter.convert_genbank_to_sbol3(
+            str(SAMPLE_GENBANK_FILE_2),
+            TEST_OUTPUT_SBOL3,
+            namespace=TEST_NAMESPACE,
+            write=False,
+        )
         sbol3_file_2 = sbol3.Document()
-        sbol3_file_2.read(location=SAMPLE_SBOL3_FILE_2, file_format=sbol3.SORTED_NTRIPLES)
-        assert not doc_diff(test_output_sbol3, sbol3_file_2), \
-            f'Converted SBOL3 file: {TEST_OUTPUT_SBOL3} not identical to expected file: {SAMPLE_SBOL3_FILE_2}'
+        sbol3_file_2.read(
+            location=str(SAMPLE_SBOL3_FILE_2), file_format=sbol3.SORTED_NTRIPLES
+        )
+        assert not doc_diff(
+            test_output_sbol3, sbol3_file_2
+        ), f"Converted SBOL3 file: {TEST_OUTPUT_SBOL3} not identical to expected file: {SAMPLE_SBOL3_FILE_2}"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
