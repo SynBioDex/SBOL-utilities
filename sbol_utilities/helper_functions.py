410aa85bfc295487c9814b10858a5ba03e11ed89
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2022-07-21 11:32:44 +0100

    Update is circular to accept LocalSubComponent and ExternallyDefined
    Co-authored-by: Jacob Beal <jakebeal@gmail.com>

--- sbol_utilities/helper_functions.py
+++ sbol_utilities/helper_functions.py
@@ -358,7 +358,7 @@ def outgoing_links(doc: sbol3.Document) -> set[URIRef]:
         doc.traverse(collector)
     return outgoing
 
-def is_circular(obj: Union[sbol3.Component, sbol3.Feature]) -> bool:
+def is_circular(obj: Union[sbol3.Component, sbol3.LocalSubComponent, sbol3.ExternallyDefined]) -> bool:
     """Check if an SBOL Component or Feature is circular.
     :param obj: design to be checked
     :return: true if circular
