ae0c6088ae01272780388c0ebbe9a04bc474559a
Yehuda Binik <ybinik1@gmail.com>
2023-11-04 15:23:54 -0400

    Implemented converstion of collection between SBOL2 and SBOL3 for the new converter

--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -91,6 +91,44 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
             doc2_loop.write(tmp2)
             self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'sbol_3to2_implementation.xml')))
 
+    def test_3to2_collection_conversion(self):
+        """Test ability to convert a collection from SBOL3 to SBOL2"""
+        # Load an SBOL3 document and check its contents
+        doc3 = sbol3.Document()
+        doc3.read(TEST_FILES / 'sbol3_collection.nt')
+        # Convert to SBOL2 and check contents
+        doc2 = convert3to2(doc3, True)
+        #report = doc2.validate()
+        #self.assertEqual(len(report), 0, f'Validation failed: {report}')
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmp2 = Path(tmpdir) / 'doc2.xml'
+            doc2.write(tmp2)
+            self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'sbol_3to2_collection.xml')))
+            doc3_loop = convert2to3(doc2, use_native_converter=True)
+            self.assertEqual(len(doc3_loop.validate()), 0)
+            tmp3 = Path(tmpdir) / 'doc3_loop.nt'
+            doc3_loop.write(tmp3)
+            self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'sbol3_collection.nt')))
+
+    def test_2to3_collection_conversion(self):
+        """Test ability to convert a collection from SBOL2 to SBOL3"""
+        # Load an SBOL2 document and check its contents
+        doc2 = sbol2.Document()
+        doc2.read(TEST_FILES / 'sbol_3to2_collection.xml')
+        # Convert to SBOL3 and check contents
+        doc3 = convert2to3(doc2, use_native_converter=True)
+        self.assertEqual(len(doc3.validate()), 0)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tmp3 = Path(tmpdir) / 'doc3.nt'
+            doc3.write(tmp3)
+            self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'sbol3_collection.nt')))
+            doc2_loop = convert3to2(doc3, True)
+            # report = doc2.validate()
+            # self.assertEqual(len(report), 0, f'Validation failed: {report}')
+            tmp2 = Path(tmpdir) / 'doc2_loop.xml'
+            doc2_loop.write(tmp2)
+            self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'sbol_3to2_collection.xml')))
+
 
 if __name__ == '__main__':
     unittest.main()
