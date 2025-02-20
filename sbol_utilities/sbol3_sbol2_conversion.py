39d937349772e878f38481a74bef7ae23910faa3
Bryan Bartley <bryan.a.bartley@raytheon.com>
2025-02-20 11:19:01 -0800

    Create stub Sequence

--- sbol_utilities/sbol3_sbol2_conversion.py
+++ sbol_utilities/sbol3_sbol2_conversion.py
@@ -589,7 +589,11 @@ class SBOL2To3ConversionVisitor:
         elif r2.parent.parent.sequence:
             seq_ref = r2.parent.parent.sequence.identity
         else:
-            seq_ref = sbol3.PYSBOL3_MISSING
+            cdef = r2.parent.parent
+            ns = self._sbol3_namespace(cdef)
+            seq_stub = sbol3.Sequence(f'{ns}/{cdef.displayId}Seq/', namespace=ns)
+            cdef.sequence = seq_stup
+            cdef.doc.add(seq_stub)
         r3 = sbol3.Range(seq_ref, r2.start, r2.end)
         self._convert_identified(r2, r3)
         return r3
