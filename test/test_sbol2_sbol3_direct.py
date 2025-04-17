5ad5e37d473c2051e727d67e4e1c8a268149d61d
Felipe Buson <55743903+fxbuson@users.noreply.github.com>
2025-04-17 15:42:48 +0100

    Added expected SBOL3 file
    Added empty Component Definition file and enabled the test for it

--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -160,7 +160,7 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
             with tempfile.TemporaryDirectory() as tmpdir:
                 tmp3 = Path(tmpdir) / 'doc3.nt'
                 doc3.write(tmp3)
-                #self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'BBa_J23101_patched.nt')))
+                self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'empty_componentDefinition.nt')))
                 doc2_loop = convert3to2(doc3, True)
                 # report = doc2.validate()
                 # self.assertEqual(len(report), 0, f'Validation failed: {report}')
@@ -177,7 +177,7 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
             with tempfile.TemporaryDirectory() as tmpdir:
                 tmp3 = Path(tmpdir) / 'doc3.nt'
                 doc3.write(tmp3)
-                #self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'BBa_J23101_patched.nt')))
+                self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'empty_componentDefinition_nover.nt')))
                 doc2_loop = convert3to2(doc3, True)
                 # report = doc2.validate()
                 # self.assertEqual(len(report), 0, f'Validation failed: {report}')
