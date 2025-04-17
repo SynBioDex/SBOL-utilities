f276fc9c56a4428cf310a1ab78a0a24c587911e0
Felipe Buson <55743903+fxbuson@users.noreply.github.com>
2025-04-17 10:57:43 +0100

    Restored implementation test
    Adapted existing implementation file since we don't have a SBOL Notebooks version

--- sbol_utilities/sbol3_sbol2_conversion.py
+++ sbol_utilities/sbol3_sbol2_conversion.py
@@ -229,7 +229,7 @@ class SBOL3To2ConversionVisitor:
     def visit_implementation(self, imp3: sbol3.Implementation):
         # Priority: 1
         # Make the Implement object and add it to the document
-        imp2 = sbol2.Implementation(imp3.identity, version=self._sbol2_version(imp3))
+        imp2 = sbol2.Implementation(self._sbol2_identity(imp3), version=self._sbol2_version(imp3))
         imp2.built = imp3.built
         self.doc2.addImplementation(imp2)
         # Map over all other TopLevel properties and extensions not covered by the constructor
@@ -553,7 +553,7 @@ class SBOL2To3ConversionVisitor:
     def visit_implementation(self, imp2: sbol2.Implementation):
         # Priority: 1
         # Make the Implementation object and add it to the document
-        imp3 = sbol3.Implementation(imp2.identity, namespace=self._sbol3_namespace(imp2), built=imp2.built)
+        imp3 = sbol3.Implementation(self._sbol3_identity(imp2), namespace=self._sbol3_namespace(imp2), built=imp2.built)
         self.doc3.add(imp3)
         # Map over all other TopLevel properties and extensions not covered by the constructor
         self._convert_toplevel(imp2, imp3)
--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -187,6 +187,27 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
                 tmp2 = Path(tmpdir) / 'doc2_loop.xml'
                 doc2_loop.write(tmp2)
                 self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'minimal_sbol2_collection.xml')))
+
+                
+    def test_sbol2_implementation(self):
+            """Test ability to convert a collection of simple parts from SBOL2 to SBOL3"""
+            # Depends on working Component Definition conversion
+            # Load an SBOL2 document and check its contents
+            doc2 = sbol2.Document()
+            doc2.read(TEST_FILES / 'sbol_3to2_implementation_compliant.xml')
+            # Convert to SBOL3 and check contents
+            doc3 = convert2to3(doc2, use_native_converter=True)
+            self.assertEqual(len(doc3.validate()), 0)
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmp3 = Path(tmpdir) / 'doc3.nt'
+                doc3.write(tmp3)
+                #self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'BBa_J23101_patched.nt')))
+                doc2_loop = convert3to2(doc3, True)
+                # report = doc2.validate()
+                # self.assertEqual(len(report), 0, f'Validation failed: {report}')
+                tmp2 = Path(tmpdir) / 'doc2_loop.xml'
+                doc2_loop.write(tmp2)
+                self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'sbol_3to2_implementation_compliant.xml')))
                 
 if __name__ == '__main__':
     unittest.main()
