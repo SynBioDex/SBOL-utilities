55d58b482c19d71b9f1ffc2265f6ef1933bb2202
Felipe Buson <55743903+fxbuson@users.noreply.github.com>
2025-04-17 10:03:30 +0100

    Added test and test file
    Added test and test file for ComponentDefinition.
    I don't know why this wasn't commited earlier

--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -149,5 +149,24 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
             self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'sbol_3to2_collection.xml')))
     '''
 
+    def test_empty_componentDefinition(self):
+            """Test ability to convert a simple part from SBOL2 to SBOL3"""
+            # Load an SBOL2 document and check its contents
+            doc2 = sbol2.Document()
+            doc2.read(TEST_FILES / 'empty_componentDefinition.xml')
+            # Convert to SBOL3 and check contents
+            doc3 = convert2to3(doc2, use_native_converter=True)
+            self.assertEqual(len(doc3.validate()), 0)
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmp3 = Path(tmpdir) / 'doc3.nt'
+                doc3.write(tmp3)
+                #self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'BBa_J23101_patched.nt')))
+                doc2_loop = convert3to2(doc3, True)
+                # report = doc2.validate()
+                # self.assertEqual(len(report), 0, f'Validation failed: {report}')
+                tmp2 = Path(tmpdir) / 'doc2_loop.xml'
+                doc2_loop.write(tmp2)
+                self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'empty_componentDefinition.xml')))
+                
 if __name__ == '__main__':
     unittest.main()
