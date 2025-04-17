172bccbd8a4dbed9753bc5963af1fe444253f5b5
Felipe Buson <55743903+fxbuson@users.noreply.github.com>
2025-04-17 14:00:56 +0100

    Added test for Component Definition with a sequence
    Also fixed description for implementation test

--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -207,8 +207,7 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
 
                 
     def test_sbol2_implementation(self):
-            """Test ability to convert a collection of simple parts from SBOL2 to SBOL3"""
-            # Depends on working Component Definition conversion
+            """Test ability to convert an implementation from SBOL2 to SBOL3"""
             # Load an SBOL2 document and check its contents
             doc2 = sbol2.Document()
             doc2.read(TEST_FILES / 'sbol_3to2_implementation_compliant.xml')
@@ -225,6 +224,26 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
                 tmp2 = Path(tmpdir) / 'doc2_loop.xml'
                 doc2_loop.write(tmp2)
                 self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'sbol_3to2_implementation_compliant.xml')))
+
+    def test_seq_componentDefinition(self):
+            """Test ability to convert a Component Definition with a Sequence from SBOL2 to SBOL3"""
+            # Depends on working Component Definition conversion
+            # Load an SBOL2 document and check its contents
+            doc2 = sbol2.Document()
+            doc2.read(TEST_FILES / 'seq_componentDefinition.xml')
+            # Convert to SBOL3 and check contents
+            doc3 = convert2to3(doc2, use_native_converter=True)
+            self.assertEqual(len(doc3.validate()), 0)
+            with tempfile.TemporaryDirectory() as tmpdir:
+                tmp3 = Path(tmpdir) / 'doc3.nt'
+                doc3.write(tmp3)
+                #self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / 'BBa_J23101_patched.nt')))
+                doc2_loop = convert3to2(doc3, True)
+                # report = doc2.validate()
+                # self.assertEqual(len(report), 0, f'Validation failed: {report}')
+                tmp2 = Path(tmpdir) / 'doc2_loop.xml'
+                doc2_loop.write(tmp2)
+                self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / 'seq_componentDefinition.xml')))
                 
 if __name__ == '__main__':
     unittest.main()
