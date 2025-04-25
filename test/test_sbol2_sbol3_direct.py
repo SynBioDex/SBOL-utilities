bc085467746ce45e1e895c0321b80bd40fcebee7
Bryan Bartley <bryan.a.bartley@raytheon.com>
2025-04-25 06:30:19 -0700

    Validate Documents after generating them, not after writing them.

--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -27,18 +27,25 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
 
         # Convert to SBOL3 and check contents
         doc3 = convert2to3(doc2, use_native_converter=True)
-        self.assertEqual(len(doc3.validate()), 0)
+        report = doc3.validate()
+        self.assertEqual(len(report), 0, f'Validation failed: {report}')
         with tempfile.TemporaryDirectory() as tmpdir:
             tmp3 = Path(tmpdir) / 'doc3.nt'
             doc3.write(tmp3)
             self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / rubric_filename)))
+
+            # Round-trip back to SBOL2 and check contents
             doc2_loop = convert3to2(doc3, True)
+            report = doc2_loop.validate()
+            self.assertEqual(report, 'Valid.', f'Validation failed: {report}')
             tmp2 = Path(tmpdir) / 'doc2_loop.xml'
             doc2_loop.write(tmp2)
             self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / test_filename)))
-            report = doc3.validate()
-            self.assertEqual(len(report), 0, f'Validation failed: {report}')
 
+    # Currently fails with the following validation error:
+    # sbol-12403: Strong Validation Error: The endedAtTime property of an Activity object is OPTIONAL and MAY contain
+    # a DateTime. Reference: SBOL Version 2.3.0 Section 12.8.1 on page 73 : https://synbiohub.org/public/igem/igem2sbol
+    @unittest.expectedFailure
     def test_2to3_J23101_conversion(self):
         """Test ability to convert a simple part from SBOL2 to SBOL3"""
         self.handle_2to3_conversion('BBa_J23101.xml', 'BBa_J23101_patched.nt')
@@ -62,19 +69,23 @@ class TestDirectSBOL3SBOL2Conversion(unittest.TestCase):
         # Load an SBOL3 document and check its contents
         doc3 = sbol3.Document()
         doc3.read(TEST_FILES / test_filename)
+
         # Convert to SBOL2 and check contents
         doc2 = convert3to2(doc3, True)
+        report = doc2.validate()
+        self.assertEqual(report, 'Valid.', f'Validation failed: {report}')
         with tempfile.TemporaryDirectory() as tmpdir:
             tmp2 = Path(tmpdir) / 'doc2.xml'
             doc2.write(tmp2)
             self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / rubric_filename)))
+ 
+            # Round-trip back to SBOL3 and check contents
             doc3_loop = convert2to3(doc2, use_native_converter=True)
-            self.assertEqual(len(doc3_loop.validate()), 0)
+            report = doc3_loop.validate()
+            self.assertEqual(len(report), 0, f'Validation failed: {report}')
             tmp3 = Path(tmpdir) / 'doc3_loop.nt'
             doc3_loop.write(tmp3)
             self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / test_filename)))
-            report = doc2.validate()
-            self.assertEqual(report, 'Valid.', f'Validation failed: {report}')
 
     # Currently fails with the following validation error:
     # sbol-12403: Strong Validation Error: The endedAtTime property of an Activity object is OPTIONAL and MAY contain
