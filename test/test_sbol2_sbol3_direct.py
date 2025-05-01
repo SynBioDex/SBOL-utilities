10b14a630d38036308ceaa2792f8b3c604f66b21
Bryan Bartley <bryan.a.bartley@raytheon.com>
2025-04-30 20:42:14 -0700

    Catch validation error

--- test/test_sbol2_sbol3_direct.py
+++ test/test_sbol2_sbol3_direct.py
@@ -28,7 +28,7 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
         # Convert to SBOL3 and check contents
         doc3 = convert2to3(doc2, use_native_converter=True)
         report = doc3.validate()
-        self.assertEqual(len(report), 0, f'Validation failed: {report}')
+        self.assertEqual(len(report), 0, report)
         with tempfile.TemporaryDirectory() as tmpdir:
             tmp3 = Path(tmpdir) / 'doc3.nt'
             doc3.write(tmp3)
@@ -37,18 +37,25 @@ class TestDirectSBOL2SBOL3Conversion(unittest.TestCase):
             # Round-trip back to SBOL2 and check contents
             doc2_loop = convert3to2(doc3, True)
             report = doc2_loop.validate()
-            self.assertEqual(report, 'Valid.', f'Validation failed: {report}')
+            self.assertEqual(report, 'Valid.', report)
             tmp2 = Path(tmpdir) / 'doc2_loop.xml'
             doc2_loop.write(tmp2)
             self.assertFalse(file_diff(str(tmp2), str(TEST_FILES / test_filename)))
 
-    # Currently fails with the following validation error:
-    # sbol-12403: Strong Validation Error: The endedAtTime property of an Activity object is OPTIONAL and MAY contain
-    # a DateTime. Reference: SBOL Version 2.3.0 Section 12.8.1 on page 73 : https://synbiohub.org/public/igem/igem2sbol
-    @unittest.expectedFailure
     def test_2to3_J23101_conversion(self):
         """Test ability to convert a simple part from SBOL2 to SBOL3"""
-        self.handle_2to3_conversion('BBa_J23101.xml', 'BBa_J23101_patched.nt')
+        try:
+            self.handle_2to3_conversion('BBa_J23101.xml', 'BBa_J23101_patched.nt')
+        except AssertionError as e:
+            self.assertIn(
+               (
+                  'Invalid. sbol-12403: Strong Validation Error: The endedAtTime property of an '
+                  'Activity object is OPTIONAL and MAY contain a DateTime. Reference: SBOL Version 2.3.0 Section '
+                  '12.8.1 on page 73 : https://synbiohub.org/public/igem/igem2sbol  Validation failed.'
+               ),
+               str(e).replace('\x00',''),
+               str(e)
+            )
 
     def test_2to3_implementation_conversion(self):
         """Test ability to convert an Implementation from SBOL2 to SBOL3"""
@@ -87,13 +94,20 @@ class TestDirectSBOL3SBOL2Conversion(unittest.TestCase):
             doc3_loop.write(tmp3)
             self.assertFalse(file_diff(str(tmp3), str(TEST_FILES / test_filename)))
 
-    # Currently fails with the following validation error:
-    # sbol-12403: Strong Validation Error: The endedAtTime property of an Activity object is OPTIONAL and MAY contain
-    # a DateTime. Reference: SBOL Version 2.3.0 Section 12.8.1 on page 73 : https://synbiohub.org/public/igem/igem2sbol
-    @unittest.expectedFailure
     def test_3to2_J23101_conversion(self):
         """Test ability to convert a simple part from SBOL3 to SBOL2"""
-        self.handle_3to2_conversion('BBa_J23101_patched.nt', 'BBa_J23101.xml')
+        try:
+            self.handle_3to2_conversion('BBa_J23101_patched.nt', 'BBa_J23101.xml')
+        except AssertionError as e:
+            self.assertIn(
+               (
+                  'Invalid. sbol-12403: Strong Validation Error: The endedAtTime property of an '
+                  'Activity object is OPTIONAL and MAY contain a DateTime. Reference: SBOL Version 2.3.0 Section '
+                  '12.8.1 on page 73 : https://synbiohub.org/public/igem/igem2sbol  Validation failed.'
+               ),
+               str(e).replace('\x00',''),
+               str(e)
+            )
 
     def test_3to2_implementation_conversion(self):
         """Test ability to convert an Implementation from SBOL3 to SBOL2"""
