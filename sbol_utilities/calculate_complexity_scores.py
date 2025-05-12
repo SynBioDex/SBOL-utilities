f9035ee8c94c8da410e2183b8aee66cfacf1bf09
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-05-12 16:44:44 +0530

    implement dynamic subdomain logic for IDTAccountAccessor

--- sbol_utilities/calculate_complexity_scores.py
+++ sbol_utilities/calculate_complexity_scores.py
@@ -66,12 +66,14 @@ class IDTAccountAccessor:
         logging.info('Connecting to IDT API')
         data = {'grant_type': 'password', 'username': self.username, 'password': self.password, 'scope': 'test'}
         auth = HTTPBasicAuth(self.client_id, self.client_secret)
-        result = post(IDTAccountAccessor._TOKEN_URL, data, auth=auth, timeout=IDTAccountAccessor.SCORE_TIMEOUT)
 
-        if 'access_token' in result.json():
-            return result.json()['access_token']
-        else:
-            raise ValueError('Access token for IDT API could not be generated. Check your credentials.')
+        for domain in IDTAccountAccessor._SUBDOMAINS:
+            result = post(f'{domain}{IDTAccountAccessor._TOKEN_ENDPOINT}', data, auth=auth, timeout=IDTAccountAccessor.SCORE_TIMEOUT)
+            if result.status_code == 200:
+                self.base_url = domain
+                return result.json()['access_token']
+
+        raise ValueError('Access token for IDT API could not be generated. Check your credentials.')
 
     def get_sequence_scores(self, sequences: list[sbol3.Sequence]) -> list:
         """Retrieve synthesis complexity scores of sequences from the IDT API
@@ -91,7 +93,7 @@ class IDTAccountAccessor:
         results = []
         for idx, partition in enumerate(partitions_sequences):
             logging.debug('Sequence score request %i of %i', idx+1, len(partitions_sequences))
-            resp = post(IDTAccountAccessor._SCORE_URL, json=partition, timeout=IDTAccountAccessor.SCORE_TIMEOUT,
+            resp = post(f'{self.base_url}{IDTAccountAccessor._SCORE_ENDPOINT}', json=partition, timeout=IDTAccountAccessor.SCORE_TIMEOUT,
                         headers={'Authorization': 'Bearer {}'.format(self.token),
                                  'Content-Type': 'application/json; charset=utf-8'})
             response_list = resp.json()
